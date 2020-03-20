32114729675c523dc12065cef041cafa10b2762d
杜晓东 (Smith Du) <307420929@qq.com>
2020-03-20 17:29:50 +0800

    mock && 登录页&& vuex && utils 等

--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'normalize.css/normalize.css' // CSS样式重置
+import '@/styles/index.scss' // global css
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'development') {
+  const {
+    mockXHR
+  } = require('../mock')
+  mockXHR()
+}
+
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -25,6 +25,7 @@ module.exports = {
         port: 8077,
         https: false,
         hotOnly: false,
+        before: require('./mock/mock-server.js')
         /* 使用代理 */
         // proxy: { //配置自动启动浏览器
         //     "/XX/*": {
@@ -40,6 +41,7 @@ module.exports = {
         //         secure: false
         //     },
         // }
+
     },
     // webpack 配置，键值对象时会合并配置，为方法时会改写配置
     // https://cli.vuejs.org/guide/webpack.html#simple-configuration
@@ -64,40 +66,50 @@ module.exports = {
             // 为开发环境修改配置...
             config.plugins = [...config.plugins, ...pluginsDev];
         }
-
     },
     chainWebpack: (config) => {
         // 链式配置
         config.resolve.alias.set('styles', resolve('src/assets/styles'))
+            .set("@", resolve("src"))
+        // set preserveWhitespace
+        config.module
+            .rule('vue')
+            .use('vue-loader')
+            .loader('vue-loader')
+            .tap(options => {
+                options.compilerOptions.preserveWhitespace = true
+                return options
+            })
+            .end()
     },
-    // css相关配置
-    css: {
-        // 启用 CSS modules
-        requireModuleExtension: false,
-        // 是否使用css分离插件 ExtractTextPlugin
-        extract: true,
-        // 开启 CSS source maps?
-        sourceMap: false,
-        // css预设器配置项
-        loaderOptions: {
-            sass: {
-                //设置css中引用文件的路径，引入通用使用的scss文件（如包含的@mixin）
-                data: `
-				$baseUrl: "/";
-				@import '@/assets/scss/_common.scss';
-				`
-            }
+    // // css相关配置
+    // css: {
+    //     // 启用 CSS modules
+    //     requireModuleExtension: true,
+    //     // 是否使用css分离插件 ExtractTextPlugin
+    //     extract: true,
+    //     // 开启 CSS source maps?
+    //     sourceMap: true,
+    //     // css预设器配置项
+    //     loaderOptions: {
+    //         // sass: {
+    //         //     //设置css中引用文件的路径，引入通用使用的scss文件（如包含的@mixin）
+    //         //     data: `
+    // 		// 	$baseUrl: "/";
+    // 		// 	@import '@/assets/scss/_common.scss';
+    // 		// 	`
+    //         // }
 
-        },
-    },
+    //     },
+    // },
     // 第三方插件配置 https://www.npmjs.com/package/vue-cli-plugin-style-resources-loader
-    pluginOptions: {
-        'style-resources-loader': { //https://github.com/yenshih/style-resources-loader
-            preProcessor: 'scss', //声明类型
-            'patterns': [
-                //path.resolve(__dirname, './src/assets/scss/_common.scss'), 
-            ],
-            //injector: 'append'
-        }
-    }
+    // pluginOptions: {
+    //     'style-resources-loader': { //https://github.com/yenshih/style-resources-loader
+    //         preProcessor: 'scss', //声明类型
+    //         'patterns': [
+    //             //path.resolve(__dirname, './src/assets/scss/_common.scss'), 
+    //         ],
+    //         //injector: 'append'
+    //     }
+    // }
 }
